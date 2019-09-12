0cf582bf7977921ff449aaffb0f2e0bee8705224
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-09-12 09:25:23 -0700

    did the questons

--- MySrcipt1.js
+++ MySrcipt1.js
@@ -1,9 +1,31 @@
   $(document).ready(function(){
 	$(".1").click(function(){
-		$(".1").toggle();//.css("background-color", "green"
+		$(".1").css("background-color", "green");
 	});
 	$(".2").click(function(){
-		$(".2").toggle();//.css("background-color", "green"
+		$(".2").css("background-color", "red");
 	});
 	
-});
+	$(".3").click(function(){
+		$(".3").css("background-color", "green");
+	});
+	$(".4").click(function(){
+		$(".4").css("background-color", "red");
+	});
+	
+	$(".5").click(function(){
+		$(".5").css("background-color", "green");
+	});
+	$(".6").click(function(){
+		$(".6").css("background-color", "red");
+	});
+	
+	$(".7").click(function(){
+		$(".7").css("background-color", "green");
+	});
+	$(".8").click(function(){
+		$(".8").css("background-color", "red");
+	});
+	
+	});
+	
